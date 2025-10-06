d3fa8b709935cfb2258d768ad6777047ff9949d2
David <ashuaib1991@gmail.com>
2025-10-06 21:16:43 +0100

    concatenated again

--- app/api/_lib/backend.js
+++ app/api/_lib/backend.js
@@ -1,5 +1,5 @@
 export const BACKEND =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
   (process.env.NODE_ENV === "production"
-    ? "https://fastfolderbackend.fly.dev"
+    ? "https://fastfolderbackend.fly.devs"
     : "http://localhost:5000");
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,5 @@
 export const API_URL =
-process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+process.env.NEXT_PUBLIC_API_URLs || "http://localhost:5000";
 
 // Helper to generate full image URL
 export const getImageUrl = (fileName) => `${API_URL}/uploads/${fileName}`;
